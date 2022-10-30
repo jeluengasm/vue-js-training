bd21d7bfa3732591c99c6ea3d9a156fd95733183
Jhon Luengas <jeluengasm@unal.edu.co>
2022-10-30 12:22:20 -0500

    feat(01-JS-review): Template string

---
+++ 01-js-review/src/bases/02-template-string.js
@@ -0,0 +1,8 @@
+const firstname = 'Han'
+const lastname = 'Solo'
+
+const fullname = `${firstname} ${lastname}` // Template string
+
+
+console.log(fullname)
+console.log(`Result 1 + 2 = ${1 + 2}`) // We can put operations and functions
--- 01-js-review/src/index.js
+++ 01-js-review/src/index.js
@@ -1,3 +1,5 @@
-import './bases/01-const-let.js';
+// import './bases/01-const-let.js'
+import './bases/02-template-string.js'
+
 
 console.log('Hello')
