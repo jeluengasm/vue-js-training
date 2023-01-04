9827831a22232d5eba4fa2cda55ac5151bd63b7c
Jhon Luengas <jeluengasm@unal.edu.co>
2023-01-04 17:49:30 -0500

    Component state - data

--- 02-vue-fundamentals/app.js
+++ 02-vue-fundamentals/app.js
@@ -3,6 +3,13 @@ const app = Vue.createApp({
     // <h1>Hello world</h1>
     // <p>{{ [{a:1},{b:2},{c:3},{d:4}] }}</p>
     // `
+    data() { // Create variables using Options API
+        return {
+            message: 'Hello world using Vue.js',
+            author: 'Bruce Wayne',
+
+        }
+    },
 })
 
 app.mount('#myApp')
