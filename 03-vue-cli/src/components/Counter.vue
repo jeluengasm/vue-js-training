c921bd57145edc0bbfe02a9a30a0b808c8cb2402
Jhon Luengas <jeluengasm@unal.edu.co>
2023-01-05 00:13:02 -0500

    Computed properties

---
+++ 03-vue-cli/src/components/Counter.vue
@@ -0,0 +1,31 @@
+<template>
+  <h2> Counter </h2>
+  <p>{{ counter }} <sup>2</sup> = {{ getSquareValue() }}</p>
+  <p>{{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
+  <p>{{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
+  <p>{{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            counter: 5
+        }
+    },
+    methods: { // Traditional method. Doesn't uses cache
+        getSquareValue() {
+            return this.counter * this.counter
+        }
+    },
+    computed: { // Uses cache
+        squareCounter() {
+            return this.counter * this.counter
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
