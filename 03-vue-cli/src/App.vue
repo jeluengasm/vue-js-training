ec6582e9e78afd677494befed0245c9f8af98b48
Jhon Luengas <jeluengasm@unal.edu.co>
2023-02-19 23:32:48 -0500

    Increase, decrease

--- 03-vue-cli/src/App.vue
+++ 03-vue-cli/src/App.vue
@@ -1,7 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <Counter/>
-
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Counter/>
 
 </template>
 
@@ -9,20 +8,20 @@
 import Counter from './components/Counter.vue';
 
 export default {
-  name: 'App',
-  components: {
-    Counter
-  }
+    name: 'App',
+    components: {
+        Counter
+    }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
--- 03-vue-cli/src/components/Counter.vue
+++ 03-vue-cli/src/components/Counter.vue
@@ -1,31 +1,49 @@
 <template>
-  <h2> Counter </h2>
-  <p>{{ counter }} <sup>2</sup> = {{ getSquareValue() }}</p>
-  <p>{{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
-  <p>{{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
-  <p>{{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
+    <h2> Counter </h2>
+    <p>{{ counter }}</p>
+    <div>
+        <span>
+            <button @click="increaseButton">+ 1</button> <!-- Same that v-on:click -->
+        </span>
+        <span>
+            <button @click="decreaseButton">- 1</button>
+        </span>
+    </div>
 </template>
 
 <script>
 export default {
+    name: 'Counter',
     data() {
         return {
-            counter: 5
+        counter: 0
         }
     },
-    methods: { // Traditional method. Doesn't uses cache
-        getSquareValue() {
-            return this.counter * this.counter
-        }
-    },
-    computed: { // Uses cache
-        squareCounter() {
-            return this.counter * this.counter
+    methods: {
+        increaseButton() {
+            this.counter++;
+        },
+        decreaseButton() {
+            this.counter--;
         }
     }
 }
 </script>
 
 <style>
+    button {
+        background-color: #4CAF50;
+        border-radius: 5px;
+        border: 1px solid white;
+        color: white;
+        cursor: pointer;
+        margin: 0 5px;
+        padding: 5px 15px;
+        transition: 0.3s ease-in-out;
+    }
 
+    button:hover {
+        background-color: #3e8e41;
+        transition: 0.3s ease-in-out;
+    }
 </style>
