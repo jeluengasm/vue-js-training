20a719de2625e859d5b1c23109252f48bd9975e9
Jhon Luengas <jeluengasm@unal.edu.co>
2023-03-14 23:27:07 -0500

    First frontend unit test

---
+++ 05-intro-test/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <Counter v-bind:start="10*10" book-title="Entered"/>
+    <Counter :start="10" book-title="Entered 2"/> -->  <!-- Same as v-bind:start -->
+    <Indecision/>
+
+</template>
+
+<script>
+// import Counter from '@/components/Counter.vue';
+import Indecision from '@/components/Indecision.vue';
+
+export default {
+    name: 'App',
+    components: {
+        // Counter,
+        Indecision
+    }
+}
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
---
+++ 05-intro-test/src/components/Counter.vue
@@ -0,0 +1,64 @@
+<template>
+    <h2> {{ customTitle }} </h2>
+    <p>{{ counter }}</p>
+    <div>
+        <span>
+            <button @click="increaseButton">+ 1</button> <!-- Same as v-on:click -->
+        </span>
+        <span>
+            <button @click="decreaseButton">- 1</button>
+        </span>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        bookTitle: String,
+        start: {
+            type: Number,
+            required: true,
+            default: 100,
+            validator(value) {
+                return value > 9
+            }
+        },
+    },
+    data() {
+        return {
+            counter: this.start
+        }
+    },
+    methods: {
+        increaseButton() {
+            this.counter++;
+        },
+        decreaseButton() {
+            this.counter--;
+        }
+    },
+    computed: {
+        customTitle() {
+            return this.bookTitle || 'Counter';
+        }
+    }
+}
+</script>
+
+<style>
+    button {
+        background-color: #4CAF50;
+        border-radius: 5px;
+        border: 1px solid white;
+        color: white;
+        cursor: pointer;
+        margin: 0 5px;
+        padding: 5px 15px;
+        transition: 0.3s ease-in-out;
+    }
+
+    button:hover {
+        background-color: #3e8e41;
+        transition: 0.3s ease-in-out;
+    }
+</style>
